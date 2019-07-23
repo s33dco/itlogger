3581e1ecbeb1ca21d5af85137674cebb71d69aee
s33dco <code@s33d.co>
2019-07-23 23:27:06 +0100

    import and initialize materialize Icons and JSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 const App = () => {
+	useEffect(() => {
+		// Init Materialize JS
+		M.AutoInit();
+	});
 	return <div className='App'>IT Logger</div>;
 };
 
